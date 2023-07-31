3e4c115e9c9405b62ed7a49c886ce60a267c412b
Andras Kucsma <r0maikx02b@gmail.com>
2023-07-31 18:02:44 +0200

    Add glm to workflow

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p build
 cd build
 
-em++ ../haste-proto/*.cpp -s DISABLE_EXCEPTION_CATCHING=1 -s USE_GLFW=3 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=0 -s ASSERTIONS=1 -Os
+em++ ../haste-proto/*.cpp -s DISABLE_EXCEPTION_CATCHING=1 -s USE_GLFW=3 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=0 -s ASSERTIONS=1 -Os -Iglm
 
 cp ../index.html .
 
